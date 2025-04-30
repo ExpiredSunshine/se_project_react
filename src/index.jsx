d2a111504e7b3eeb8a1328c12265ba41bdcc6345
A.J. Croft <acroft41@yahoo.com>
2025-04-30 18:02:36 -0500

    add font styles and color

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App/App.jsx";
 import "./vendor/normalize.css";
+import "./vendor/fonts/fonts.css";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
