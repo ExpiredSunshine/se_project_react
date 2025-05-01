864aa2700073d14d67af39d63ad861985f3b872e
A.J. Croft <acroft41@yahoo.com>
2025-04-30 19:14:15 -0500

    implement and style header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,22 @@
+import React from "react";
 import "./Header.css";
 import logo from "../../assets/logo.png";
 import avatar from "../../assets/avatar.png";
 
 function Header() {
+  const currentDate = new Date().toLocaleString("default", {
+    month: "long",
+    day: "numeric",
+  });
+  const locationName = "New York";
+
   return (
     <header className="header">
-      <img src={logo} className="header__logo" />
-      <p className="header__date-and-location">DATE, LOCATION</p>
-      <button className="header__add-clothes-btn"> + Add Clothes</button>
+      <img src={logo} className="header__logo" alt="Logo" />
+      <p className="header__date-and-location">
+        {currentDate}, {locationName}
+      </p>
+      <button className="header__add-clothes-btn">+ Add Clothes</button>
       <div className="header__user-container">
         <p className="header__username">User Name</p>
         <img src={avatar} alt="Avatar" className="header__avatar" />
