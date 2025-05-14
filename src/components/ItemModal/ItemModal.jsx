ea7dd229432b516f955054131442b27370ba67e2
A.J. Croft <acroft41@yahoo.com>
2025-05-13 21:35:12 -0500

    style item card delete button

--- src/components/ItemModal/ItemModal.jsx
+++ src/components/ItemModal/ItemModal.jsx
@@ -11,9 +11,17 @@ function ItemModal({ activeModal, card, handleCloseClick, onDelete }) {
         ></button>
         <img src={card.imageUrl} alt={card.name} className="modal__image" />
         <div className="modal__footer">
-          <h2 className="modal__caption">{card.name}</h2>
-          <p className="modal__weather">Weather: {card.weather}</p>
-          <button onClick={() => onDelete(card)}>Delete</button>
+          <div className="modal__info">
+            <h2 className="modal__caption">{card.name}</h2>
+            <p className="modal__weather">Weather: {card.weather}</p>
+          </div>
+          <button
+            type="button"
+            className="modal__item-delete"
+            onClick={() => onDelete(card)}
+          >
+            Delete item
+          </button>
         </div>
       </div>
     </div>
