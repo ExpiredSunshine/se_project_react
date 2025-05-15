d279a0097de3a50741f07f2b1fc86ade66e9142a
A.J. Croft <acroft41@yahoo.com>
2025-05-15 16:48:33 -0500

    implement footer date logic

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="footer">
       <p className="footer__signature">Developed by A.J. Croft</p>
-      <p className="footer__date">2025</p>
+      <p className="footer__date">{new Date().getFullYear()}</p>
     </footer>
   );
 }
